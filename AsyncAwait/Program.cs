598e219d8619bca17050fb17a3aa8dfcf429252b
Indu Sree <Indu Sree@DESKTOP-UN8GH21>
2024-08-19 10:05:02 +0530

    Add project files.

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Starting.....");
+
+            await DoWorkAsync();
+
+            Console.WriteLine("Completed");
+        }
+
+        static async Task DoWorkAsync()
+        {
+            Console.WriteLine("Wait, Task in Progress");
+
+            await Task.Delay(1000);
+
+            Console.WriteLine("Task Completed");
+        }
+    }
+}
